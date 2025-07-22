cddf289f4e29f3522d9cbcd2b126ff03b0a98312
Kui Ádám <adam.kui@icloud.com>
2025-07-22 11:51:01 +0200

    Split router, improve procedures

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,11 +1,26 @@
 import { trpc } from "./utils/trpc";
 
 function App() {
-  const hello = trpc.hello.useQuery({ name: "Világ" });
+  const user = trpc.user.getUser.useQuery();
+  const hello = trpc.user.greetUser.useQuery({ name: user.data?.name ?? "" });
+  const mutation = trpc.message.saveMessage.useMutation();
+
+  const sendMessage = () => {
+    mutation.mutate({ message: "Ez egy próba" });
+  };
 
   if (hello.isLoading) return <p>Betöltés...</p>;
 
-  return <h1>{hello.data?.message}</h1>;
+  if (mutation.isSuccess) {
+    console.log(mutation.data.message);
+  }
+
+  return (
+    <>
+      <h1>{hello.data?.message}</h1>
+      <button onClick={sendMessage}>Üzenet küldése</button>
+    </>
+  );
 }
 
 export default App;
--- packages/trpc/src/index.ts
+++ packages/trpc/src/index.ts
@@ -3,12 +3,31 @@ import { z } from "zod";
 
 const t = initTRPC.create();
 
-export const appRouter = t.router({
-  hello: t.procedure
+// GET = trpc query
+// POST / PUT / DELETE = trpc mutation
+
+const userRouter = t.router({
+  getUser: t.procedure.query(() => ({ id: 1, name: "Teszt Elek" })),
+  greetUser: t.procedure
     .input(z.object({ name: z.string() }))
     .query(({ input }) => {
       return { message: `Hello, ${input.name}!` };
     }),
+});
+
+const messageRouter = t.router({
+  saveMessage: t.procedure
+    .input(z.object({ message: z.string() }))
+    .mutation(({ input }) => {
+      const message = `Üzenet elküldve: ${input.message}`;
+      console.log(message);
+      return { message };
+    }),
+});
+
+export const appRouter = t.router({
+  user: userRouter,
+  message: messageRouter,
 });
 
 export type AppRouter = typeof appRouter;
